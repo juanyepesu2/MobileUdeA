1bfb230fbc852572ec06999cb32cc80fb7b083ad
Affigueroab <affigueroab@unal.edu.co>
2021-10-13 06:09:30 -0500

    feat: Se consume la información del Json desde un repositorio virtual.

--- app/src/main/java/com/mintic/mobileudea/data.remote/RetrofitFactory.kt
+++ app/src/main/java/com/mintic/mobileudea/data.remote/RetrofitFactory.kt
@@ -1,21 +1,7 @@
 package com.mintic.mobileudea.data.remote
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
 object RetrofitFactory {
 
     private const val BASE_URL = "https://my-json-server.typicode.com/juanyepesu2/MobileUdeA/"
 
-    private val gson: Gson = GsonBuilder()
-        .setLenient()
-        .create()
 
-    private fun retrofit(): Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create(gson))
-        .build()
-
-    fun apiService(): ApiService = retrofit().create(ApiService::class.java)
 }
